5997fb60eca4cbe065a2d99712e91fe87b11fba1
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2026-03-20 18:59:12 -0300

    Ocultando Bearer token

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,13 +1,10 @@
 describe('Api Adopet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjZGVlZTE3Yy1lYjQ4LTQwM2MtODc2My00NzdlOTY4Y2Q3MzkiLCJhZG9wdGVyTmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNzc0MDI4ODM4LCJleHAiOjE3NzQyODgwMzh9.kTDvEklK824k_AMAs56xqzF8Bta1Ygy8nh1sVaet8Yk`
 
     it('Mensagens da API', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/cdeee17c-eb48-403c-8763-477e968cd739',
-            headers: {
-                Authorization: authorization
-            }
+            headers: cypress.env()
         }).then((response) => {
             expect(response.status).to.eq(200)
             expect(response.body).is.not.empty
@@ -16,17 +13,16 @@ describe('Api Adopet', () => {
     })
 
     it('John Doe', () => {
-              cy.request({
-                  method: 'GET' ,
-                  url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/cdeee17c-eb48-403c-8763-477e968cd739',
-                  headers: { authorization }
-                 
-              }).then((res) => {
-                  expect(res.status).to.be.equal(200)
-                  expect(res.body).is.not.empty
-                  expect(res.body).to.have.property('perfil')
-              expect(res.body.perfil.nome).to.be.equal('John Doe')
-                               
-              })
-          })
+        cy.request({
+            method: 'GET' ,
+            url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/cdeee17c-eb48-403c-8763-477e968cd739',
+            headers: { authorization }
+            
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('perfil')
+            expect(res.body.perfil.nome).to.be.equal('John Doe')                     
+        })
+    })
 })
